20789d028f4495c53469726c278c9b413b73b0bd
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2024-10-07 20:45:48 +0530

    commit mongodb

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,11 +1,23 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
+
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World");
 });
 
-app.listen(port, () => {
-  console.log("Server is listneing on port : " + port);
-});
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose Connected");
+    app.listen(port, () => {
+      console.log("Server is listening on port: " + port);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
