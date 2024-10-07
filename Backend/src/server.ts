982fe7c7df9382ca4d4fb676b392e051b24ec24a
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2024-10-07 21:57:03 +0530

    added singleton to create database instance

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,16 +2,43 @@ import app from "./app";
 import mongoose from "mongoose";
 import env from "./util/validateEnv";
 
-const port = env.PORT;
+class Database {
+  private static instance: Database;
+  private isConnected: boolean = false;
 
-mongoose
-  .connect(env.MONGO_CONNECTION_STRING)
-  .then(() => {
-    console.log("Mongoose Connected");
-    app.listen(port, () => {
-      console.log("Server is listening on port: " + port);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error.message);
-  });
+  private constructor() {
+    // Private constructor to prevent direct instantiation
+  }
+
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+
+  public connect(): void {
+    if (this.isConnected) {
+      console.log("Database is already connected.");
+      return;
+    }
+
+    mongoose
+      .connect(env.MONGO_CONNECTION_STRING)
+      .then(() => {
+        this.isConnected = true;
+        console.log("Mongoose Connected");
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB:", error.message);
+      });
+  }
+}
+
+const port = env.PORT;
+const database = Database.getInstance();
+database.connect();
+
+app.listen(port, () => {
+  console.log("Server is listening on port: " + port);
+});
