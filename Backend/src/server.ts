be71a236a8aebe9a644703f3a809165e891012f7
James Sembukuttiarachchi <jamessanjayasem@gmail.com>
2024-10-07 21:37:58 +0530

    validate env. apply mvc

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,17 +1,11 @@
-import "dotenv/config";
+import app from "./app";
 import mongoose from "mongoose";
-import express from "express";
+import env from "./util/validateEnv";
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
-
-const port = process.env.PORT;
+const port = env.PORT;
 
 mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING!)
+  .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
     console.log("Mongoose Connected");
     app.listen(port, () => {
